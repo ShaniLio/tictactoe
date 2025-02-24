c9cf9ee8de8e2f1a262c2bfaa823ac4cbe8baa3d
ShaniLio <shanilio112@gmail.com>
2025-02-24 18:00:48 +0500

    Add missing TikTacToe component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import TikTacToe from './Components/tiktactoe'
+import TikTacToe from './Components/TikTacToe'
+// import TikTacToe from './Components/TikTacToe'
 
 function App() {
   const [count, setCount] = useState(0)
