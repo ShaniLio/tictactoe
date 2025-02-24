b33c5122361188698bf869ec7d7f72f7e0d72417
ShaniLio <shanilio112@gmail.com>
2025-02-24 18:06:36 +0500

    changing in file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import TikTacToe from './Components/TikTacToe'
+import TikTacToe from '../Components/TikTacToe'
 // import TikTacToe from './Components/TikTacToe'
 
 function App() {
